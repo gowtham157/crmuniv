6bf08dbc072a1f3de735ffcb60fa0b41df5d536b
gowtham157 <109079683+gowtham157@users.noreply.github.com>
2023-09-30 15:19:54 +0100

    Update RegistrationForm.js
    version 1.3

--- RegistrationForm.js
+++ RegistrationForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import { TextField, Button, Container, Grid, MenuItem } from '@mui/material';
 
-const RegistrationForm = () => {
+const RegistrationForm = ({ onApiResponse }) => {
   const [studentData, setStudentData] = useState({
     name: '',
     email: '',
     courses: [],
     universities: [],
   });
+
+  const [selectedCourse, setSelectedCourse] = useState('');
+  const [selectedUniversity, setSelectedUniversity] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -15,29 +19,29 @@ const RegistrationForm = () => {
 
   const handleCourseChange = (e) => {
     const { value } = e.target;
-    // Ensure at least three courses are selected
-    if (studentData.courses.length < 3) {
-      setStudentData({ ...studentData, courses: [...studentData.courses, value] });
-    }
+    setSelectedCourse(value);
   };
 
   const handleUniversityChange = (e) => {
     const { value } = e.target;
-    // Ensure at least three universities are selected
-    if (studentData.universities.length < 3) {
-      setStudentData({ ...studentData, universities: [...studentData.universities, value] });
-    }
+    setSelectedUniversity(value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      const updatedStudentData = {
+        ...studentData,
+        courses: [...studentData.courses, selectedCourse],
+        universities: [...studentData.universities, selectedUniversity],
+      };
+
       const response = await fetch('http://3.90.145.92:8081/crmuniv/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(studentData),
+        body: JSON.stringify(updatedStudentData),
       });
 
       if (!response.ok) {
@@ -46,41 +50,88 @@ const RegistrationForm = () => {
 
       const data = await response.json();
       console.log(data); // Handle the response data as needed
+      // Pass the response data to the parent component
+      onApiResponse(data);
     } catch (error) {
       console.error(error.message);
     }
   };
 
+  // Sample course and university options (replace with actual options)
+  const sampleCourses = ['Course 1', 'Course 2', 'Course 3', 'Course 4'];
+  const sampleUniversities = ['University A', 'University B', 'University C', 'University D'];
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
-        <input type="text" name="name" onChange={handleChange} required />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" name="email" onChange={handleChange} required />
-      </div>
-      <div>
-        <label>Select Courses (At least 3):</label>
-        <select name="courses" onChange={handleCourseChange} multiple>
-              <option value="Course 1">Masters in Computer Science</option>
-              <option value="Course 2">Masters in Cyber Security</option>
-              <option value="Course 3">Masters in Software Engineering</option>
-              <option value="Course 4">Masters in Business Administration</option>
-        </select>
-      </div>
-      <div>
-        <label>Select Universities (At least 3):</label>
-        <select name="universities" onChange={handleUniversityChange} multiple>
-			        <option value="University 1">University of Leeds</option>
-              <option value="University 2">University of Liverpool</option>
-              <option value="University 3">University of Leicester</option>
-              <option value="University 4">University of Northampton</option>
-        </select>
-      </div>
-      <button type="submit">Register</button>
-    </form>
+    <Container maxWidth="md">
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Name"
+              name="name"
+              variant="outlined"
+              value={studentData.name}
+              onChange={handleChange}
+              required
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              type="email"
+              variant="outlined"
+              value={studentData.email}
+              onChange={handleChange}
+              required
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              select
+              label="Select Course"
+              name="course"
+              variant="outlined"
+              value={selectedCourse}
+              onChange={handleCourseChange}
+              required
+            >
+              {sampleCourses.map((course) => (
+                <MenuItem key={course} value={course}>
+                  {course}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              select
+              label="Select University"
+              name="university"
+              variant="outlined"
+              value={selectedUniversity}
+              onChange={handleUniversityChange}
+              required
+            >
+              {sampleUniversities.map((university) => (
+                <MenuItem key={university} value={university}>
+                  {university}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" color="primary">
+              Register
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
   );
 };
 
